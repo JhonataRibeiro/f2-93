e2205b9b286fbda81e2f45ddcfe0e2cb961d7f74
= <jhonata.ribeiro.ti@gmail.com>
2016-02-09 14:18:26 -0200

    update class SequenciaDeElementosHashSet

--- src/br/com/project/SequenciaElementosHashSet.java
+++ src/br/com/project/SequenciaElementosHashSet.java
@@ -1,5 +1,12 @@
 package br.com.project;
 
 public class SequenciaElementosHashSet {
-
+	private HashSet colecao ;
+	public SequenciaElementosHashSet(HashSet colecao){
+		this.colecao = colecao;
+	}
+	
+	public Object proximo(){
+		return null;
+	}
 }
